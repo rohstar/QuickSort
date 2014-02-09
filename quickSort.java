13435a18b86796c9416eb84a3dda7ccb7eef716c
rohstar <rohstar@gmail.com>
2014-02-09 16:01:10 -0500

    Added QuickSort To Test

---
+++ quickSort.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class quickSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		List<Integer> l = new ArrayList<Integer>();
+		l.add(10); l.add(7); l.add(13); l.add(22); l.add(9);
+		//l.get(2);
+		
+		System.out.println(l.get(2));
+	    //System.out.println(sort(l));
+		
+		
+	}
+
+	
+	public static List<Integer> sort(List<Integer> l) {
+
+		//if (l.size() < 2) {
+		//	return l;
+		//}
+
+		int pivot = l.get(0);
+		List<Integer> left = new ArrayList<Integer>();
+		List<Integer> right = new ArrayList<Integer>();
+
+		for (int i = 0; i < l.size(); i++) {
+			if (l.get(i) < pivot) {
+				left.add(l.get(i));
+				//System.out.println(left);
+			} else {
+				right.add(l.get(i));
+			}
+
+		}
+		return join(sort(left), sort(right), pivot);
+	}
+
+	public static List<Integer> join(List<Integer> left, List<Integer> right, int pivot) {
+
+		List<Integer> l =  new ArrayList<Integer>();
+
+		for (int i = 0; i < left.size(); i++) {
+			l.add(left.get(i));
+		}
+		
+		l.add(pivot);
+		
+		for (int i = l.size(); i < right.size(); i++) {
+			l.add(right.get(i));
+		}
+		return l;
+	}
+	
+}
